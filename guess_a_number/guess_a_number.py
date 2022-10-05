c5d9600855ad6c9fa5120c97829e297c33fa18a0
Jerald Bledsoe <jeraldeb@outlook.com>
2022-10-05 01:32:40 -0700

    Added function to allow the user to change the range of numbers to choose from (such as 1-100 or 1-1000) Added function to keep track of the number of attempts needed Added function "Play again?" option at the end Added a leader board/levels that tracks successes

--- guess_a_number/guess_a_number.py
+++ guess_a_number/guess_a_number.py
@@ -1,4 +1,9 @@
 
+#Allow the user to change the range of numbers to choose from (such as 1-100 or 1-1000)
+#Keep track of the number of attempts needed
+#Add a "Play again?" option at the end
+#Add a leader board that tracks successes
+
 import random
 import time
 
@@ -11,6 +16,34 @@ def GetDifficultyMode():
             return mode
         else:
             print("Invalid mode")
+           
+# Function to get starting number.
+def GetCustomStartingNumber(first_number):         
+    while True:
+        print("Please select a starting number between 0 and 100.")
+        print("Default number is: " + str(first_number))
+        integer = GetIntegerOnlyInput()
+        if integer >= 0 and integer <= 100:
+            return integer
+        else:
+            print("Not cool man. Invalid number..")
+            
+# Function to get ending number that's higher than the starting number.                 
+def GetCustomEndingNumber(first_number, second_number):                  
+    while True:
+        print("Please select a number between 0 and 100.")
+        if second_number > first_number:
+            print("Default number is: " + str(second_number))
+            
+        print("This number must be greater than the starting number.")
+        integer = GetIntegerOnlyInput()
+        if integer >= 0 and integer <= 100:
+            if integer > first_number:
+                return integer
+            else:
+                print("Don't play me. Pick a different number.")
+        else:
+            print("Not cool man. Invalid number..")
             
 # Function to get the integer guess with validation.
 def GetIntegerOnlyInput():
@@ -50,38 +83,72 @@ def PrintRandomGuessString(guess):
         print(random.choice(["Lower my guy..", "Nope! Too high!", "Shooting for the moon are we?"]))
     else:
         print(random.choice(["Higher my guy..", "Nope! Too low!", "Digging a hole?"]))
-    
+
 # Function to print three dots in 1 second intervals.
 def PrintWaitDots():
-    for i in range(3):
+    for i in range(2):
         print(".")
         time.sleep(1)
+        
+# Function to increment guess count if it's under 3 guesses otherwise print you lose.
+def IncrementGuessCount(game_variables, guess_count):
     
+    if guess_count < 3:
+        return guess_count + 1
+    else:
+        print("You lose!")
+        print("Thanks for playing!")
+        PrintWaitDots()
+        # Do you want to try again?
+        print("Do you want to try again? [Please type Yes or No]")
+        while True:
+            userInput = input()
+            if userInput == "Yes" or "yes":
+                main(game_variables)
+            elif userInput == "No" or "no":
+                print("Thanks for playing!")
+                exit()
+            else:
+                print("Hey.. you need to select yes or no..")
+                
 # Main Game Function
 def main(game_variables):
     
     # Let's assign each value in the list game_variables to a variable named first_number, second_number, level, and mode.
     first_number, second_number, level, mode = game_variables
+    guess_count = 0
+    guesses = 3 - guess_count
+    
     print("----------------------------------------")
     print("Welcome to level " + str(level) + "!")
+    print("You're playing on the " + mode + " difficulty mode.")
     print("Now guess a number between " + str(first_number) + " and " + str(second_number) + ".")
+    print("You only have " + str(guesses) + " guesses so choose wisely! Muhahaha")
     magic_number = random.randint(first_number, second_number)
     while True:
         guess = GetIntegerOnlyInput()
+        guesses = 3 - guess_count
         if guess == magic_number:
             PrintRandomVictoryString()
+            PrintWaitDots()
             break
         elif guess > magic_number:
+            print("----------------------------------------")
             PrintRandomGuessString("high")
+            print("You only have " + str(guesses) + " guesses.")
+            guess_count = IncrementGuessCount(game_variables, guess_count)            
         else:
+            print("----------------------------------------")
             PrintRandomGuessString("low")
+            print("You only have " + str(guesses) + " guesses.")
+            guess_count = IncrementGuessCount(game_variables, guess_count)  
             
     print("Continue to next level? [Please type Yes or No]")
     while True:
-        uInput = input()
-        if uInput == "Yes" or "yes":
+        userInput = input()
+        if userInput == "Yes" or "yes":
             main(NextLevelIncrease(game_variables))
-        elif uInput == "No" or "no":
+        elif userInput == "No" or "no":
             print("Thanks for playing!")
             break
         else:
@@ -89,6 +156,7 @@ def main(game_variables):
             
 # Initial Global Settings
 game_variables = [1,10, 0, "easy"]
+first_number, second_number, level, mode = game_variables
 
 
 # Main Code
@@ -99,5 +167,9 @@ print("----------------------------------------")
 PrintWaitDots()
 mode = GetDifficultyMode()
 PrintWaitDots()
-game_variables[-1] = mode
+first_number = GetCustomStartingNumber(first_number)
+PrintWaitDots()
+second_number = GetCustomEndingNumber(first_number, second_number)
+PrintWaitDots()
+game_variables = [first_number, second_number, level, mode]
 main(game_variables)
